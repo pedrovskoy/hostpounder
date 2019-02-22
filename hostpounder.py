93d93c907c13c068d2e7093296d99a8d661af0a8
Pedro Amorim <pedrohasa@gmail.com>
2019-02-22 16:46:26 -0300

    Update code
    Set hosts file path, add file handling, and streamline "toggle" function.

--- hostpounder.py
+++ hostpounder.py
@@ -1,31 +1,32 @@
+path = 'C:\\Windows\\System32\\Drivers\\etc\\hosts'
 query = 'spclient.wg.spotify.com'
-        
-def find(hosts, query):
-    for line in hosts:
+
+with open(path, "r") as hosts_file:
+    data = hosts_file.readlines()
+    
+def find(data, query):
+    """Returns query's index in the file"""
+    for line in data:
         if query in line:
-            q_index = hosts.index(line)
-            break
+            q_index = data.index(line)
     return(q_index)
 
-t_index = find(hosts, query)
+t_index = find(data, query)
 
-def toggle(hosts, t_index):
-    target = hosts[t_index]
+def toggle(data, t_index):
+    """ Adds or remove hash character
+    as first character in the line """
+    target = data[t_index]
     if target.startswith('#'):
         target_update = target[1:]
-        hosts[t_index] = target_update
+        data[t_index] = target_update
         print("Toggled off.\n")
-    elif target.startswith('0') or target.startswith('1'):
+    else:
         target_update = '#' + target
-        hosts[t_index] = target_update
+        data[t_index] = target_update
         print("Toggled on.\n")
-    return(hosts)
+    return(data)
 
-def toggle_caller(hosts):
-    run = input("Enter 'q' to quit\n")
-    while run.lower() != 'q':
-        toggle(hosts, t_index)
-        print("Control: {}".format(hosts[0]))
-        run = input("Enter 'q' to quit.\n")
-
-toggle_caller(hosts)
+with open(path, "w") as hosts_file:
+    toggle(data, t_index)
+    hosts_file.writelines(data)
