741430b2756cbd27c4d987a525ac0c85f962d282
Pedro Amorim <pedrohasa@gmail.com>
2019-03-24 21:59:10 -0300

    Add Linux compatibility and improve readability

--- hostpounder.py
+++ hostpounder.py
@@ -2,23 +2,30 @@ import ctypes
 import fileinput
 import sys
 
-def is_admin():
-    """Check whether program is running with admin privileges"""
-    try:
-        return ctypes.windll.shell32.IsUserAnAdmin()
-    except:
-        return False
-
-if is_admin():
-    path = 'C:\\Windows\\System32\\Drivers\\etc\\hosts'
-else:
-    ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, __file__, None, 1)
-
 query = 'spclient.wg.spotify.com'
 
+# Detect platform
+if sys.platform.startswith('win32'):
+    def is_admin():
+        """Check whether program is running with admin privileges"""
+        try:
+            return ctypes.windll.shell32.IsUserAnAdmin()
+        except:
+            return False
+
+    if is_admin():
+        path = 'C:\\Windows\\System32\\Drivers\\etc\\hosts'
+    else:
+        ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, __file__, None, 1)
+
+elif sys.platform.startswith('linux'):
+    path = '/etc/hosts'
+
+# Open hosts file
 with open(path, "r") as hosts_file:
     data = hosts_file.readlines()
-    
+
+# Define functions to locate query and toggle hash character
 def find(data, query):
     """Return index of query in the file"""
     for line in data:
@@ -26,8 +33,6 @@ def find(data, query):
             query_index = data.index(line)
             break
     return query_index
-
-target_index = find(data, query)
 
 def toggle(data, target_index):
     """ Add or remove hash character
@@ -43,6 +48,10 @@ def toggle(data, target_index):
         print("Toggled on.\n")
     return data
 
+# Call find() to retrieve the target index
+target_index = find(data, query)
+
+# Write changes to hosts file
 with open(path, "w") as hosts_file:
     toggle(data, target_index)
     hosts_file.writelines(data)
